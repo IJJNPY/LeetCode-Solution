9b3221eec43a75412cc6d038aabb7c86aa7377d5
IJJNPY <Jiwen-Ma@outlook.com>
2023-05-24 02:56:23 +0800

    Modification to LeetCode1_TwoSum.java

--- src/LeetCode1_TwoSum.java
+++ src/LeetCode1_TwoSum.java
@@ -2,9 +2,13 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum {
+public class LeetCode1_TwoSum {
+    //LeetCode1
+    //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    //You can return the answer in any order.
     public static void main(String[] args) {
-        TwoSum soultion = new TwoSum();
+        LeetCode1_TwoSum soultion = new LeetCode1_TwoSum();
         int[] nums = {2,7,11,15};
         int target = 9;
         System.out.println(Arrays.toString(soultion.twoSumup(nums,target)));
